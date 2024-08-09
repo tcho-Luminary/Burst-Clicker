490ff6b10ab24da455d6ba2abd58f47328984871
tcho-Luminary <tcholuminary@gmail.com>
2024-08-09 17:21:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
-import clicker
-import keyboard_listener
+import pyautogui
+import keyboard
+import time
+import random
+from pynput.mouse import Listener
 
-p = print
-p("Welcome to burst Clicker")
-p("press R to activate/deactivate double clicker")
+print("Welcome to burst Clicker!")
+print("Type 'R' to activate double clicker")
+print("Write 'Clicker' to activate clicker and write your cps eg; 12-18")
 
-keyboard_listener.start()
+def get_cps_input():
+    cps_range = input("Enter CPS range (eg; 12-18): ")
+    cps_range = cps_range.split("-")
+    return int(cps_range[0]), int(cps_range[1])
+
+def on_click(x, y, button, pressed):
+    if pressed and button == mouse.Button.left:
+        if keyboard.read_key() == "u":
+            pyautogui.click()
+
+def autoclick(cps_range):
+    cps_min, cps_max = cps_range
+    while True:
+        if keyboard.is_pressed("r"):
+            break
+
+
+def main():
+    while True:
+        command = input("Enter command: ")
+        if command == "r":
+            autoclick(get_cps_input())
+        elif command == "clicker":
+            autoclick(get_cps_input())
+        else:
+            print("Invalid command. Try again!")
+
+if __name__ == "__main__":
+    main()
+
+###
+
+
